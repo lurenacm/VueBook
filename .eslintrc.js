9ed2aad8bdcee8eff55d896f6f8fb475ccc9d9df
HWfuture <18320398437@139.com>
2020-09-12 21:36:24 +0800

    finish fontsize

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'index': 'off'
+    'index': 'off',
+    'space-before-function-paren': 0,
+    "quotes": [0, "double"],
+    // "semi": [2, "off"],
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <span class="icon-bookmark"></span>
+    <span class="icon-close"></span>
+    <span class="text">aaasss</span>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  const html = document.querySelector('html')
+  html.style.fontSize = fontSize + 'px'
+})
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./assets/styles/global.scss";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.text {
+  font-family: "Days One";
+  color: blanchedalmond;
+  font-size: 90px;
 }
 </style>
